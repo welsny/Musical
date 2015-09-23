1fb39c096e6b771810a762bf344819c69f2a95c6
Zeng, Wesley(wnzeng) <wnzeng@paypal.com>
2015-09-23 12:59:21 -0400

    add unit testing

--- test_module.py
+++ test_module.py
@@ -1,12 +1,27 @@
 __author__ = 'wnzeng'
 
-import unittest, Identifier
+import unittest
+
+import Identifier as id
 
 class TestBackend(unittest.TestCase):
-    """
-    Integration test of Identifier.py, Octave.py, and Chord.py
-    """
-    def test_piano(self):
-        pass
-    def test_guitar(self):
-        pass
+
+    def test_identify(self):
+
+        self.assertEqual("A min", id.identify("x02210"))
+
+        self.assertEqual("E dom7", id.identify("xx2434"))
+
+        self.assertEqual("C Maj", id.identify("x32010"))
+
+        self.assertEqual("C Maj7", id.identify("x32000"))
+
+        self.assertEqual("A#/Bb min", id.identify("A# C# E#"))
+
+        self.assertEqual("D Maj", id.identify("D F# A"))
+
+        self.assertEqual("Unknown Chord", id.identify("x93010"))
+
+        self.assertEqual("B dom7", id.identify("x21202"))
+
+unittest.main()
