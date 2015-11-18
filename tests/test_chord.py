c3c671bf159d6dd97fb9ea983d97ea760042a93b
welsny <zengw@mssm.org>
2015-11-18 02:34:30 -0500

    refactor test_module.py into an acutal test module

---
+++ tests/test_chord.py
@@ -0,0 +1,17 @@
+import unittest
+from path import *
+from chord import *
+
+
+class Test(unittest.TestCase):
+
+    def test_chord(self):
+
+	    c = Note('C', 0)
+	    cmaj = Chord('Maj', [0, 4, 7], c)
+
+	    self.assertEqual(cmaj, Chord('Maj', [0, 4, 7]).with_root(c))
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_identifier.py
+++ tests/test_identifier.py
@@ -1,12 +1,11 @@
-from identifier import *
-from octave import *
-from chord import *
 import unittest
+from path import *
+from identifier import *
 
 
 class Test(unittest.TestCase):
 
-    def test_integration(self):
+    def test_identify(self):
 
         self.assertEqual("C Maj", identify("x32010"))
 
@@ -23,21 +22,6 @@ class Test(unittest.TestCase):
         self.assertEqual("Unknown Chord", identify("x93010"))
 
         self.assertEqual("B dom7", identify("x21202"))
-
-    def test_init(self):
-
-        self.assertEqual("C Maj", str(Octave([0, 4, 7]).chord))
-
-        self.assertEqual("D Maj", str(Octave([2, 6, 9]).chord))
-
-        self.assertEqual("A min", str(Octave([9, 4, 0]).chord))
-
-    def test_chord(self):
-
-        c = Note('C', 0)
-        cmaj = Chord('Maj', [0, 4, 7], c)
-
-        self.assertEqual(cmaj, Chord('Maj', [0, 4, 7]).with_root(c))
 
 
 if __name__ == "__main__":
